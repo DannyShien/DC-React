3b780993e41522ac4cf29711f1ee7de474a0cbd3
Stanley Chen <dannyshien@gmail.com>
2018-12-03 10:44:42 -0500

    give each player a card display

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -3,13 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    // add a comment
+    constructor(props) {
+        super(props);
+
+        //Set up state
+        this.state = {
+            scores: [
+                {
+                    id: 1, 
+                    name: 'ahjumah', 
+                    score: 1001
+                },
+                {
+                    id: 2, 
+                    name: 'ahjassi', 
+                    score: 20
+                },
+                {
+                    id: 3, 
+                    name: 'stinkface', 
+                    score: 500
+                } 
+            ]
+        }
+    }
+
   render() {
     return (
       <div className="App">
-
+        <h1>Scoreboard App</h1>
+        <div className='score-container'> 
+            {this._scoresAsCards()}
+        </div>
       </div>
     );
+  }
+
+  _scoresAsCards() {
+      const cards = this.state.scores.map(score => {
+          return (
+              <div key= {score.id}>
+                  <h2>
+                      Name: {score.name}
+                        <br />
+                      Score: {score.score}  
+                  </h2>
+              </div>
+          );
+      });
+      return cards;
   }
 }
 
