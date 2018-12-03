0a85d47369acae664391312b8f3bdffff99ac5ce
Stanley Chen <dannyshien@gmail.com>
2018-12-03 10:13:56 -0500

    initial commit for react practice

---
+++ counter/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Yo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+// Equivalent to module.exports = App;
