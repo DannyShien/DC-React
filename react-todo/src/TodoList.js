97a8fca2ae5f3eb3306fa3884b7a20c27c0606e6
Stanley Chen <dannyshien@gmail.com>
2018-12-03 14:46:53 -0500

    initial commit for a react todo app

---
+++ react-todo/src/TodoList.js
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+
+
+
+class TodoList extends Components {
+    render () {
+        return (
+            <div>
+                <h1>React Todo App</h1>
+            </div>
+        ) 
+    }
+}
+
+
+export default TodoList;
