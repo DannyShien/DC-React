321c7cac33a94d6fb6c299269d11275f25f03109
Stanley Chen <dannyshien@gmail.com>
2018-12-03 11:28:44 -0500

    increment score count for button

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -28,31 +28,57 @@ class App extends Component {
         }
     }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Scoreboard App</h1>
-        <div className='score-container'> 
-            {this._scoresAsCards()}
-        </div>
-      </div>
-    );
-  }
-
-  _scoresAsCards() {
-      const cards = this.state.scores.map(score => {
-          return (
-              <div key= {score.id}>
-                  <h2>
-                      Name: {score.name}
-                        <br />
-                      Score: {score.score}  
-                  </h2>
+    render() {
+      return (
+        <div className="App">
+            <h1>Scoreboard App</h1>
+                <div className='score-container'> 
+                    {this._scoresAsCards()}
+                </div>
               </div>
-          );
-      });
-      return cards;
-  }
+        );
+    }
+
+    _scoresAsCards() {
+        const cards = this.state.scores.map(score => {
+            return (
+                <div key= {score.id}>
+                    <h2>
+                        Name: {score.name}
+                            <br />
+                        Score: {score.score}  
+                    </h2>
+                    <button onClick={() => this._incrementScoreById(score.id)}>+</button>
+                </div>
+            );
+        });
+        return cards;
+    }
+
+
+    // Version 1 .map, manaully constructing replacement
+    _incrementScoreById(id) {
+        // console.log('Adding a score')
+        // find the player in this.state.scores
+        // increment their score
+        const newScores = this.state.scores.map(score => {
+            if(score.id !== id) {
+                return score
+            } else {
+                return {
+                    id: score.id, 
+                    name: score.name, 
+                    score: score.score + 1
+                }
+            }
+        })
+        // and call this.setState
+        this.setState({
+            scores: newScores
+        })
+    }
+
+    
 }
 
 export default App;
