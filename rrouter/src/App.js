0639ec48934ab7d366ea7ce6726aa68b5f9dbcdf
Stanley Chen <dannyshien@gmail.com>
2018-12-11 15:12:09 -0500

    adding second object to fitness

--- rrouter/src/App.js
+++ rrouter/src/App.js
@@ -1,70 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import {
-    BrowserRouter as Router, // Watches for changes to URL
-    Route,                   // Renders components based on the specific URL
-    // Link                     // Changes the URL so the Router sees it
-} from 'react-router-dom';
-
-import NavBar from './NavBar';
-import Home from './Home';
-import About from './About';
 import Fitness from './Fitness';
-import WorkOuts from './WorkOuts';
+
 
 
 class App extends Component {
-    constructor (props) {
-        super(props); 
-        this.state = {
-            getSwole: {
-                'Chest': [
-                    'Bench Press', 
-                    'Cable Chest Flys', 
-                    'Incline Press',
-                    'Push-Ups'
-                ],
-                'Back': [
-                    'Pull-Ups',
-                    'Seated Rows', 
-                    'Lat Pull-Downs',
-                    'Reverse Flys'
-                ],
-                'Shoulders': [
-                    'Military Press',
-                    'Side Raises', 
-                    'Seated Incline Front Raises',
-                    'Single Arm Power Clean'
-                ],
-                'Legs': [
-                    'Dead Lifts',
-                    'Leg Press', 
-                    'Lunges', 
-                    'Calve Raises'
-                ]
-            }
-        };
-    }
+    render() {
+        return (
+            <div className="App">
+                <Fitness />
 
-  render() {
-    return (
-        <Router>
-            <div>
-                <NavBar />
-                <Route path = '/' exact component = {Home} />
-                <Route path = '/about' component = {About} />
-                <Route path = '/fitness' render = {(props) => {
-                    return <Fitness bodySection = {Object.keys(this.state.getSwole)} {...props} />
-                }} />
-                <Route path = '/fitness/:workOutType' render = {(props) => {
-                    return <WorkOuts bodyPart = {this.state.getSwole} {...props} />
-                }} />
             </div>
-        </Router>
-    );
-  }
+            
+        );
+    }                          
+
+
 }
+
 
 export default App;
--- rrouter/src/Fitness.js
+++ rrouter/src/Fitness.js
@@ -1,25 +1,120 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component} from 'react';
 
-const Fitness = (props) => {
+import {
+    BrowserRouter as Router, // Watches for changes to URL
+    Route,                   // Renders components based on the specific URL
+    // Link                     // Changes the URL so the Router sees it
+} from 'react-router-dom';
 
-    const bodyExercises = props.bodySection.map(workOutType => {
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import Exercises from './Exercises';
+import WorkOuts from './WorkOuts';
+
+
+class Fitness extends Component {
+    constructor (props) {
+        super(props); 
+        this.state = {
+            getSwole: {
+                'Chest': [
+                    'Bench Press', 
+                    'Cable Chest Flys', 
+                    'Incline Press',
+                    'Push-Ups'
+                ],
+                'Back': [
+                    'Pull-Ups',
+                    'Seated Rows', 
+                    'Lat Pull-Downs',
+                    'Reverse Flys'
+                ],
+                'Shoulders': [
+                    'Military Press',
+                    'Side Raises', 
+                    'Seated Incline Front Raises',
+                    'Single Arm Power Clean'
+                ],
+                'Legs': [
+                    'Dead Lifts',
+                    'Leg Press', 
+                    'Lunges', 
+                    'Calve Raises'
+                ]
+            },
+            workoutDays: {
+                'Mondays': {
+                    'Body': [
+                        'Chest', 
+                        'Tricepts', 
+                        'Abs'
+                    ],
+                    'typeOfWorkout': [
+                        'Bench Press', 
+                        'Dips',
+                        'Cable Crunch'
+                    ]
+                },
+                'Wednesdays': {
+                    'Body': [
+                        'Back', 
+                        'Bicepts', 
+                        'Abs'
+                    ],
+                    'typeOfWorkout': [
+                        'Pull-ups', 
+                        'Bicept Curls',
+                        'Sit Ups'
+                    ]
+                },
+                'Fridays': {
+                    'Body': [
+                        'Shoulders', 
+                        'Tricepts', 
+                        'Abs'
+                    ],
+                    'typeOfWorkout': [
+                        'Military Press', 
+                        'Tricept Pulldown', 
+                        'Hanging Leg Raises'
+                    ]
+                },
+                'Sundays': {
+                    'Body': [
+                        'Legs', 
+                        'Bicepts', 
+                        'Abs'
+                    ],
+                    'typeOfWorkout': [
+                        'Squats', 
+                        'More Curls', 
+                        'Planks'
+                    ]
+                }
+            }
+        };
+    }
+ 
+    render() {
         return (
-            <div>
-                <Link to = {`/fitness/${workOutType}`}>{workOutType}</Link>
-            </div>
+            <Router>
+                <div>
+                    <NavBar />
+                    <Route path = '/' exact component = {Home} />
+                    <Route path = '/about' component = {About} />
+                    <Route path = '/fitness' render = {(props) => {
+                        return <Exercises bodySection = {Object.keys(this.state.getSwole)} {...props} />
+                    }} />
+                    <Route path = '/fitness/:workOutType' render = {(props) => {
+                        return <WorkOuts bodyPart = {this.state.getSwole} {...props} />
+                    }} />
+                    
+                </div>
+            </Router>
         );
-    });
-
-    return (
-        <div>
-            <h1>Fitness</h1>
-            <ul>
-                {bodyExercises}
-            </ul>
-        </div>
-    );
-};
+    }
+}
 
 
 export default Fitness;
